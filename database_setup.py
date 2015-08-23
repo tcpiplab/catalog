da871709de2b0789c8b25ccff579db3bc2e9d2e3
Luke Sheppard <lshep.usc@gmail.com>
2015-08-22 22:32:04 -0700

    Fix indents. Add serialize() for JSON output.

--- database_setup.py
+++ database_setup.py
@@ -9,27 +9,39 @@ Base = declarative_base()
 
 # Class section
 class Restaurant(Base):
-  # Define the table called 'restaurant'
-  __tablename__ = 'restaurant'
+    # Define the table called 'restaurant'
+    __tablename__ = 'restaurant'
 
-  # Define the column mappers
-  name = Column(String(80), nullable = False)
-  id = Column(Integer, primary_key = True)
+    # Define the column mappers
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
 
 class MenuItem(Base):
-  # Define the table called 'menu_item'
-  __tablename__ = 'menu_item'
+    # Define the table called 'menu_item'
+    __tablename__ = 'menu_item'
 
-  # Define the column mappers
-  name = Column(String(80), nullable = False)
-  id = Column(Integer, primary_key = True)
-  course = Column(String(250))
-  description = Column(String(250))
-  price = Column(String(8))
-  restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-  # Create a variable representing a relationship with the Restaurant class
-  # so that our foreign key will work
-  restaurant = relationship(Restaurant)
+    # Define the column mappers
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    # Create a variable representing a relationship with the Restaurant class
+    # so that our foreign key will work
+    restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        # Returns object data in easily serializable format, i.e., JSON.
+        return {
+            'name' : self.name,
+            'description' : self.description,
+            'id' : self.id,
+            'price' : self.price,
+            'course' : self.course,
+        }
+
 
 # Ending configuration section
 ####### Insert at end of file #######
